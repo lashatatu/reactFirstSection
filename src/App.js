cc34fc82553c22158ed14e6bc043acda4818d34d
lasha tatulashvili <lashatatu@gmail.com>
2022-07-10 09:16:09 +0400

    conditionals

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
-const App = () => {
-  return (
-    <div>
-      App
-    </div>
-  );
-};
 
-export default App;
+const App = () => {
+  const title='blog post'
+  const body='this is my blog'
+
+  return (
+    <div className={'container'}>
+      <h1>{title}</h1>
+      <p>{body}</p>
+    </div>
+  )
+}
+
+export default App

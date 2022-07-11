70f3c49b771d4a7aec374e36e414cba6a8414728
lasha tatulashvili <lashatatu@gmail.com>
2022-07-11 08:19:44 +0400

    rating select component

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -4,7 +4,19 @@ import Button from './shared/Button'
 
 const FeedbackForm = () => {
   const [text, setText] = useState('')
+  const [btnDisabled, setBtnDisabled] = useState(true)
+  const [message, setMessage] = useState('')
   const handleTextChange=(e)=>{
+    if(text===''){
+      setBtnDisabled(true)
+      setMessage(null)
+    }else if(text!==''&&text.trim().length<=10){
+      setMessage('text must be at least 10 characters')
+      setBtnDisabled(true)
+    }else {
+      setMessage(null)
+      setBtnDisabled(false)
+    }
     setText(e.target.value)
   }
   return (
@@ -13,8 +25,9 @@ const FeedbackForm = () => {
         <h2>rate your service</h2>
         <div className="input-group">
           <input onChange={handleTextChange} type="text" placeholder={'write review'} value={text}/>
-          <Button type={'submit'} >Send</Button>
+          <Button type={'submit'} isDisabled={btnDisabled}>Send</Button>
         </div>
+        {message && <div className={'message'}>{message}</div>}
       </form>
     </Card>
   )

55db02d3d352307ce06e8d9a495792f09278df00
lasha tatulashvili <lashatatu@gmail.com>
2022-07-13 07:01:18 +0400

    global state

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import FeedbackStats from './components/FeedbackStats'
 import FeedbackForm from './components/FeedbackForm'
 import AboutPage from './pages/AboutPage'
 import AboutIconLink from './components/AboutIconLink'
+import { FeedbackProvider } from './context/FeedbackContext'
 
 const App = () => {
   const [feedback, setFeedback] = useState(FeedbackData)
@@ -24,25 +25,27 @@ const App = () => {
   }
   return (
     <>
-      <Router>
-        <Header/>
+      <FeedbackProvider>
+        <Router>
+          <Header/>
 
-        <div className={'container'}>
-          <Routes>
-            <Route
-              exact path={'/'} element={
-              <>
-                <FeedbackForm handleAdd={addFeedback}/>
-                <FeedbackStats feedback={feedback}/>
-                <FeedbackList feedback={feedback} handleDelete={deleteFeedback}/>
-              </>
-            }></Route>
-            <Route path={'/about'} element={<AboutPage/>}/>
-          </Routes>
-          <AboutIconLink/>
+          <div className={'container'}>
+            <Routes>
+              <Route
+                exact path={'/'} element={
+                <>
+                  <FeedbackForm handleAdd={addFeedback}/>
+                  <FeedbackStats feedback={feedback}/>
+                  <FeedbackList feedback={feedback} handleDelete={deleteFeedback}/>
+                </>
+              }></Route>
+              <Route path={'/about'} element={<AboutPage/>}/>
+            </Routes>
+            <AboutIconLink/>
 
-        </div>
-      </Router>
+          </div>
+        </Router>
+      </FeedbackProvider>
     </>
   )
 }

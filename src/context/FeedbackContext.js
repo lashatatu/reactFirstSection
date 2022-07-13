55c86dc106c05485ff2811c67e912efd528f06b1
lasha tatulashvili <lashatatu@gmail.com>
2022-07-13 07:21:19 +0400

    side effects

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -7,25 +7,51 @@ export const FeedbackProvider = ({children}) => {
   const [feedback, setFeedback] = useState([
     {
       id: 1,
-      text: 'this item is from context',
+      text: 'This is feedback item 1',
       rating: 10,
     },
+    {
+      id: 2,
+      text: 'This is feedback item 2',
+      rating: 9,
+    },
+    {
+      id: 3,
+      text: 'This is feedback item 3',
+      rating: 8,
+    },
   ])
-  const deleteFeedback = (id) => {
-    if ( window.confirm('do you want to delete') ) {
-      setFeedback(feedback.filter((item) => item.id !== id))
 
-    }
-  }
+  const [feedbackEdit, setFeedbackEdit] = useState({
+    item: {},
+    edit: false,
+  })
+
   const addFeedback = (newFeedback) => {
     newFeedback.id = uuidv4()
     setFeedback([newFeedback, ...feedback])
   }
+
+  const deleteFeedback = (id) => {
+    if ( window.confirm('do you want to delete') ) {
+      setFeedback(feedback.filter((item) => item.id !== id))
+    }
+  }
+
+  const editFeedback = (item) => {
+    setFeedbackEdit({
+      item,
+      edit: true,
+    })
+  }
+
+
   return <FeedbackContext.Provider
     value={{
       feedback,
       deleteFeedback,
-      addFeedback
+      addFeedback,
+      editFeedback
     }}>
     {children}
   </FeedbackContext.Provider>

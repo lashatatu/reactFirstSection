dd96ffeada8d3e76eca6e39f4464862716724ed5
lasha tatulashvili <lashatatu@gmail.com>
2022-07-17 08:59:47 +0400

    update delete

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -1,28 +1,35 @@
-import { createContext, useState,useEffect } from 'react'
-import { v4 as uuidv4 } from 'uuid'
+import { createContext, useState, useEffect } from 'react'
 
 const FeedbackContext = createContext()
 
 export const FeedbackProvider = ({children}) => {
-  const [isLoading,setIsLoading]=useState(true)
+  const [isLoading, setIsLoading] = useState(true)
   const [feedback, setFeedback] = useState([])
   const [feedbackEdit, setFeedbackEdit] = useState({
     item: {},
     edit: false,
   })
-  useEffect(()=>{
+  useEffect(() => {
     fetchFeedback()
-  },[])
+  }, [])
 
-  const fetchFeedback=async()=>{
-    const response=await fetch(`http://localhost:5000/feedback?_sort=id&_order=desc`)
-    const data=await response.json()
+  const fetchFeedback = async () => {
+    const response = await fetch(`/feedback?_sort=id&_order=desc`)
+    const data = await response.json()
     setFeedback(data)
     setIsLoading(false)
   }
 
-  const addFeedback = (newFeedback) => {
-    newFeedback.id = uuidv4()
+  const addFeedback = async (newFeedback) => {
+    const response = await fetch('/feedback', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(newFeedback),
+    })
+
+    const data = await response.json()
     setFeedback([newFeedback, ...feedback])
   }
 
@@ -51,7 +58,7 @@ export const FeedbackProvider = ({children}) => {
       editFeedback,
       feedbackEdit,
       updateFeedback,
-      isLoading
+      isLoading,
     }}>
     {children}
   </FeedbackContext.Provider>

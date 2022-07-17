f7cc33368877a1925b33f4489aa0745bde9b985e
lasha tatulashvili <lashatatu@gmail.com>
2022-07-17 08:53:08 +0400

    add feedback setting

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,13 +1,14 @@
 import FeedbackItem from './FeedbackItem'
 import { useContext } from 'react'
 import FeedbackContext from '../context/FeedbackContext'
+import Spinner from './shared/spinner'
 
 const FeedbackList = () => {
-  const {feedback}=useContext(FeedbackContext)
-  if ( !feedback || feedback.length === 0 ) {
+  const {feedback,isLoading}=useContext(FeedbackContext)
+  if (!isLoading&& (!feedback || feedback.length === 0 )) {
     return <p>no feedback yet</p>
   }
-  return (
+  return isLoading?<Spinner/>:(
     <div className={'feedback-list'}>
       {feedback.map((item) =>
         <FeedbackItem key={item.id} item={item} />,
